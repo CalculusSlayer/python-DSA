c62fc24ebe4afbea101670dca7454676a94e3611
Nayeel Imtiaz <naimtiaz@ucsc.edu>
2022-09-03 04:17:39 -0700

    Added search method for Trie class

--- trie/Trie.py
+++ trie/Trie.py
@@ -26,3 +26,17 @@ class Trie:
             current = node
         current.endOfString = True
         print("Successfully inserted")
+
+    def searchString(self, word):
+        currentNode = self.root
+        for i in word:
+            node = currentNode.children.get(i)
+            if node == None:
+                return False
+            currentNode = node
+        
+        if currentNode.endOfString == True:
+            return True
+        else:
+            return False
+
