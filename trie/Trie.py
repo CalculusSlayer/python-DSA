cf43102f21f520b9d718b58f2f6ae2b0e8f5f2fe
Nayeel Imtiaz <naimtiaz@ucsc.edu>
2022-09-03 03:13:46 -0700

    Started implementing trie class. Added insertString method

---
+++ trie/Trie.py
@@ -0,0 +1,28 @@
+# Nayeel Imtiaz
+# 9/3/22
+# Trie.py
+# Implementation of the `trie` data structure
+# in python.
+
+# _TrieNode not meant to be used by clients 
+# (helper class for Trie class).
+class _TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfString = False
+
+class Trie:
+    def __init__(self):
+        self.root = _TrieNode()
+    
+    def insertString(self, word):
+        current = self.root
+        for i in word:
+            ch = i
+            node = current.children.get(ch)
+            if node == None:
+                node = _TrieNode()
+                current.children.update({ch: node})
+            current = node
+        current.endOfString = True
+        print("Successfully inserted")
---
+++ trie/Trie_test.py
@@ -0,0 +1,10 @@
+import Trie as t
+
+def main():
+    NewTrie = t.Trie()
+    NewTrie.insertString ("App")
+    NewTrie.insertString ("Appl")
+
+
+if __name__ == "__main__":
+    main()
