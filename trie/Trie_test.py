0f08cfc9013e6e1270b14482faa47cd5434592fc
Nayeel Imtiaz <naimtiaz@ucsc.edu>
2022-09-03 04:17:58 -0700

    Updated to test search method

--- trie/Trie_test.py
+++ trie/Trie_test.py
@@ -5,6 +5,16 @@ def main():
     NewTrie.insertString ("App")
     NewTrie.insertString ("Appl")
 
+    print(NewTrie.searchString("App"))  # True
+    print(NewTrie.searchString("Appl")) # True
+    print(NewTrie.searchString(""))     # False
+    print(NewTrie.searchString("A"))    # False
+    print(NewTrie.searchString("Appz")) # False
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
