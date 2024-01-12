7431b63127789bdffa35428f8c6a8f0cbd9c23dd
Nayeel Imtiaz <nayeelimtiaz11@gmail.com>
2024-01-11 16:41:26 -0800

    Vanilla binary search function + test file

--- binary_search/binary.py
+++ binary_search/binary.py
@@ -1,4 +1,5 @@
 # binary.py
+import random
 
 def binary_search(array, target):
     """
@@ -11,6 +12,9 @@ def binary_search(array, target):
 
     Return:
         (int): Index of found element ([0, len(array) - 1]) or -1 if element was not found
+    
+    TC: O(log n) where n is number of elements in array
+    SC: O(1)
     """
 
     left_index, right_index = 0, len(array) - 1
@@ -26,13 +30,30 @@ def binary_search(array, target):
     return -1
 
 def main():
+    random.seed(42)
+
     array1 = [1, 2, 3, 4, 5]
     for index in range(len(array1)):
         result = binary_search(array1, array1[index])
         print(f"Array: {array1}, Target: {array1[index]}, result: {result}")
+    print(f"Array: {array1}, Target: {33}, result: {result}")
+    print()
     
-    array2 = [64]
-    print(f"Array: {array2}, Target: {64}, result: {binary_search(array2, 64)}")
+    single_element_array = [64]
+    print(f"Array: {single_element_array}, Target: {64}, result: {binary_search(single_element_array, 64)}")
+    print(f"Array: {single_element_array}, Target: {71}, result: {binary_search(single_element_array, 71)}")
+    print()
+
+    no_elements_array = []
+    print(f"Array: {no_elements_array}, Target: {24}, result: {binary_search(no_elements_array, 24)}")
+
+    random_array = sorted([random.randrange(5) for random_number in range(20)])
+    print(random_array)
+
+    print(f"Array: {random_array}, Target: {4}, result: {binary_search(random_array, 4)}")
+
+
+
 
 if __name__ == '__main__':
     main()
---
+++ binary_search/binary_test.py
@@ -0,0 +1,35 @@
+import pytest
+import time
+from binary import binary_search
+# look into the 'logging' module if
+# you want to print messages w/o
+# having to do "pytest -s" or "pytest --capture=no"
+
+def test_standard_binary_search():
+    array1 = [1, 2, 3, 4, 5]
+    for index in range(len(array1)):
+        assert binary_search(array1, array1[index]) == index
+    assert binary_search(array1, 33) == -1
+
+
+def test_single_element_binary_search():
+    single_element_array = [64]
+    assert binary_search(single_element_array, 64) == 0
+    assert binary_search(single_element_array, 71) == -1
+
+
+def test_no_elements_binary_search():
+    no_elements_array = []
+    assert binary_search(no_elements_array, 22) == -1
+
+
+def test_large_array_binary_search():
+    big_array = [number for number in range(1, int(1e6))]
+    index = 10392
+    start_time = time.time()
+    result = binary_search(big_array, big_array[10392])
+    end_time = time.time()
+
+    assert result == index, f"Expected {index} but got {result}"
+    if end_time - start_time >= 1:
+        pytest.fail(f"Binary search took too long: {end_time - start_time} seconds")
