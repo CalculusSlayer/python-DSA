402637189dfc50e6e928d38d5d98f15045a4c6bc
Nayeel Imtiaz <nayeelimtiaz11@gmail.com>
2024-10-01 15:54:52 -0700

    added union find problem

---
+++ UnionFind/oct-1-24/amazon.py
@@ -0,0 +1,14 @@
+'''
+There are N products given in pairs, the pairing indicates that they belong to the same
+category. 
+
+Return a list of product pairs so that each product in the pair does not belong to 
+the same category.
+
+Essentially make recommendations for products and make sure that the recommended product
+ is not the same category with the other product in the pair.
+
+Input: [(1,3), (2,7), (3,8)]
+Output: [(1,2),(1,7),(3,2),(3,7),(8,2),(8,7)]
+```
+
